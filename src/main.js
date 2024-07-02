dc6f349261e87e4f7b1ecfb377d467be8f576577
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-02 22:14:56 +0530

    1

---
+++ src/main.js
@@ -0,0 +1,40 @@
+//1.----------------------//
+function arithmeticOperations(a, b) {
+    let sum = a + b;
+    let difference = a - b;
+    let product = a * b;
+    let quotient = a / b;
+
+    return {
+        sum: sum,
+        difference: difference,
+        product: product,
+        quotient: quotient
+    };
+}
+
+console.log(arithmeticOperations(10, 5));
+//-----------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
